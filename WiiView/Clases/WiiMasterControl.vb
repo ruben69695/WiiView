a1de62d32284222ea219e057e635b6a097c43b21
Ruben Arrebola <ruben.arre6@gmail.com>
2018-02-05 09:06:12 +0100

    Añado nueva versión de dll y control del wii controller.

--- WiiView/Clases/WiiMasterControl.vb
+++ WiiView/Clases/WiiMasterControl.vb
@@ -22,7 +22,7 @@
         End If
     End Sub
 
-    Protected Sub CheckACliked(currentState As Boolean)
+    Public Sub CheckACliked(currentState As Boolean)
         If A = True AndAlso currentState = False Then
             A = False
             ' Función Xavi LeftUp
@@ -34,7 +34,7 @@
         End If
     End Sub
 
-    Protected Sub CheckBClicked(currentState As Boolean)
+    Public Sub CheckBClicked(currentState As Boolean)
         If B = True AndAlso currentState = False Then
             B = False
             ' Función Xavi RightUp
@@ -46,9 +46,9 @@
         End If
     End Sub
 
-    Protected Sub CheckLeftClicked(currentState As Boolean)
+    Public Sub CheckLeftClicked(currentState As Boolean)
         If Left = True AndAlso currentState = False Then
-            Left = False            
+            Left = False
         ElseIf Left = False AndAlso currentState = True Then
             Left = True
             ' Función Xavi KeyLeft
@@ -56,19 +56,19 @@
         End If
     End Sub
 
-    Protected Sub CheckRightClicked(currentState As Boolean)
+    Public Sub CheckRightClicked(currentState As Boolean)
         If Right = True AndAlso currentState = False Then
-            Right = False            
+            Right = False
         ElseIf Right = False AndAlso currentState = True Then
-            Right = true
+            Right = True
             ' Función Xavi KeyRight
             ClApiDll2.ClApi.ClickKeyRight()
         End If
     End Sub
 
-    Protected Sub CheckUpClicked(currentState As Boolean)
+    Public Sub CheckUpClicked(currentState As Boolean)
         If Up = True AndAlso currentState = False Then
-            Up = False            
+            Up = False
         ElseIf Up = False AndAlso currentState = True Then
             ' Función Xavi KeyUp
             Up = True
@@ -76,9 +76,9 @@
         End If
     End Sub
 
-    Protected Sub CheckDownClicked(currentState As Boolean)
+    Public Sub CheckDownClicked(currentState As Boolean)
         If Down = True AndAlso currentState = False Then
-            Down = False            
+            Down = False
         ElseIf Down = False AndAlso currentState = True Then
             ' Función Xavi KeyDown
             Down = True
@@ -86,10 +86,10 @@
         End If
     End Sub
 
-    Protected Sub CheckPlusClicked(currentState As Boolean)
+    Public Sub CheckPlusClicked(currentState As Boolean)
         If Perfil.Equals("P1") Then
             If Plus = True AndAlso currentState = False Then
-                Plus = False            
+                Plus = False
             ElseIf Plus = False AndAlso currentState = True Then
                 ' Función Xavi KeyPlus
                 Plus = True
@@ -98,13 +98,13 @@
         Else
             'TODO Llamar a función para modificar la sensibilidad
         End If
-        
+
     End Sub
 
-    Protected Sub CheckDashClicked(currentState As Boolean)
+    Public Sub CheckDashClicked(currentState As Boolean)
         If Perfil.Equals("P1") Then
             If Dash = True AndAlso currentState = False Then
-                Dash = False            
+                Dash = False
             ElseIf Dash = False AndAlso currentState = True Then
                 ' Función Xavi KeyDash
                 Dash = True
@@ -115,10 +115,10 @@
         End If
     End Sub
 
-    Protected Sub CheckHomeClicked(currentState As Boolean)
+    Public Sub CheckHomeClicked(currentState As Boolean)
         If Perfil.Equals("P1") Then
             If Home = True AndAlso currentState = False Then
-                Home = False            
+                Home = False
             ElseIf Home = False AndAlso currentState = True Then
                 ' Función Xavi KeyHome
                 Home = True
@@ -130,13 +130,13 @@
         End If
     End Sub
 
-    Protected Sub CheckOneClicked(currentState As Boolean)
+    Public Sub CheckOneClicked(currentState As Boolean)
         If currentState Then
             Perfil = "P1"
         End If
     End Sub
 
-    Protected Sub CheckTwoClicked(currentState As Boolean)
+    Public Sub CheckTwoClicked(currentState As Boolean)
         If currentState Then
             Perfil = "P2"
         End If
--- WiiView/WiiView.vb
+++ WiiView/WiiView.vb
@@ -7,10 +7,22 @@ Public Class WiiView
     Public chromeBrowser As ChromiumWebBrowser
     Private mWC As WiimoteCollection
     Private controllerConnected As Boolean = False
+    Private controllerConfigured As Boolean = False
     Private prevControllerConnected As Boolean = True
+    Private Delegate Sub UpdateWiimoteStateDelegate(ByVal args As WiimoteChangedEventArgs)
+    Private masterController As New WiiMasterControl()
 
     Private Sub WiiView_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
         CefSharp.Cef.Shutdown()
+    End Sub
+
+    Public Sub UpdateState(ByVal args As WiimoteChangedEventArgs)
+        BeginInvoke(New UpdateWiimoteStateDelegate(AddressOf UpdateWiimoteChanged), args)
+    End Sub
+
+    Private Sub UpdateWiimoteChanged(args As WiimoteChangedEventArgs)
+
+
     End Sub
 
     Private Sub InitializeChromium(url As String)
@@ -59,11 +71,54 @@ Public Class WiiView
                 trayIcon.BalloonTipText = "No controllers connected"
                 trayIcon.ShowBalloonTip(3000)
             End If
-        Else
-
+        ElseIf Not controllerConfigured Then
+            For Each wm As Wiimote In mWC
+                AddHandler wm.WiimoteChanged, AddressOf wm_WiimoteChanged
+                wm.Connect()
+            Next
+            controllerConfigured = True
         End If
 
         prevControllerConnected = controllerConnected
+    End Sub
+
+    Private Sub wm_WiimoteChanged(sender As Object, e As WiimoteChangedEventArgs)
+        Dim ws As WiimoteState = e.WiimoteState
+
+        Console.WriteLine(ws.ToString())
+        If ws.ButtonState.A OrElse Not ws.ButtonState.A Then
+            masterController.CheckACliked(ws.ButtonState.A)
+        End If
+        If ws.ButtonState.B OrElse Not ws.ButtonState.B Then
+            masterController.CheckBClicked(ws.ButtonState.B)
+        End If
+        If ws.ButtonState.Home OrElse Not ws.ButtonState.Home Then
+            masterController.CheckHomeClicked(ws.ButtonState.B)
+        End If
+        If ws.ButtonState.Down OrElse Not ws.ButtonState.Down Then
+            masterController.CheckDownClicked(ws.ButtonState.Down)
+        End If
+        If ws.ButtonState.Up OrElse Not ws.ButtonState.Up Then
+            masterController.CheckUpClicked(ws.ButtonState.Up)
+        End If
+        If ws.ButtonState.Left OrElse Not ws.ButtonState.Left Then
+            masterController.CheckLeftClicked(ws.ButtonState.Left)
+        End If
+        If ws.ButtonState.Right OrElse Not ws.ButtonState.Right Then
+            masterController.CheckRightClicked(ws.ButtonState.Right)
+        End If
+        If ws.ButtonState.Plus OrElse Not ws.ButtonState.Plus Then
+            masterController.CheckPlusClicked(ws.ButtonState.Plus)
+        End If
+        If ws.ButtonState.Minus OrElse Not ws.ButtonState.Minus Then
+            masterController.CheckDashClicked(ws.ButtonState.Minus)
+        End If
+        If ws.ButtonState.One OrElse Not ws.ButtonState.One Then
+            masterController.CheckOneClicked(ws.ButtonState.One)
+        End If
+        If ws.ButtonState.Two OrElse Not ws.ButtonState.Two Then
+            masterController.CheckTwoClicked(ws.ButtonState.Two)
+        End If
     End Sub
 
     Private Function IsControllerConnected() As Boolean
